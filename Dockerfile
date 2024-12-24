09b3f3b9f1030a29b2a5218ea8f50db637ef71e1
Duc Truong Huynh <38066181+huynhduc0@users.noreply.github.com>
2024-12-24 11:11:27 +0000

    fix: reduce pause duration in rollout canary configuration

--- Dockerfile
+++ Dockerfile
@@ -6,10 +6,9 @@ RUN npm install
 COPY . .
 RUN npm run build
 
-# Stage 2: Serve the application with Nginx
-FROM nginx:alpine
-COPY --from=builder /usr/src/app/dist /usr/share/nginx/html
-COPY nginx.conf /etc/nginx/nginx.conf
-
-EXPOSE 3003
-CMD ["nginx", "-g", "daemon off;"]
+# Stage 2: Run the NestJS application
+FROM arm32v7/node:16-alpine
+WORKDIR /usr/src/app
+COPY --from=builder /usr/src/app .
+EXPOSE 3000 3003
+CMD ["node", "dist/main.js"]
--- src/main.ts
+++ src/main.ts
@@ -2,17 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import config from 'config.json';
 import { WebSocketService } from './socketservice/websocket.service';
-import { Server } from 'socket.io';
-import { WsAdapter } from '@nestjs/platform-ws';
-import { IoAdapter } from '@nestjs/platform-socket.io';
 import { WebSocketServer } from 'ws';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // app.useWebSocketAdapter(new WsAdapter(app));
-  await app.listen(30001);
-  const webSocketService = app.get(WebSocketService);
+  await app.listen(3000); // Change this to 3000 to avoid conflict with WebSocket port
 
+  const webSocketService = app.get(WebSocketService);
   const defaultExchangeName = config.exchange.name;
   const defaultPair = config.exchange.pair;
 
@@ -21,20 +17,17 @@ async function bootstrap() {
   io.on('connection', (client, req) => {
     console.log('Client connected');
     const url = new URL(req.url, `http://${req.headers.host}`);
-    const exchange = url.searchParams.get('exchange') || 'default_exchange';
-    const pair = url.searchParams.get('pair') || 'default_pair';
-    if (exchange && pair) {
-      console.log(`Subscribing client to exchange: ${exchange}, pair: ${pair}`);
-      webSocketService.subscribe(client, exchange, pair);
-    } else {
-      console.log(`Subscribing client to default exchange: ${defaultExchangeName}, pair: ${defaultPair}`);
-      webSocketService.subscribe(client, defaultExchangeName, defaultPair);
-    }
+    const exchange = url.searchParams.get('exchange') || defaultExchangeName;
+    const pair = url.searchParams.get('pair') || defaultPair;
+
+    console.log(`Subscribing client to exchange: ${exchange}, pair: ${pair}`);
+    webSocketService.subscribe(client, exchange, pair);
 
     client.on('disconnect', () => {
       console.log('Client disconnected');
       webSocketService.unsubscribe(client);
     });
+
     client.on('close', () => {
       console.log('Client disconnected');
       webSocketService.unsubscribe(client);
